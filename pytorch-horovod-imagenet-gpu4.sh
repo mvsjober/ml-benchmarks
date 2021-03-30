9c97148f90c0a8124927bc4dca536bfa0ce38943
Mats Sjöberg <mats.sjoberg@csc.fi>
2021-03-30 09:49:59 +0300

    Updated PyTorch Horovod ImageNet benchmark to work with PyTorch 1.8

--- pytorch-horovod-imagenet-gpu4.sh
+++ pytorch-horovod-imagenet-gpu4.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-#SBATCH --nodes=1 --ntasks=4 --cpus-per-task=10 -p gputest --gres=gpu:v100:4,nvme:50 -t 15 --mem=32G
+#SBATCH --nodes=1 --ntasks=4 --cpus-per-task=10 -p gputest --gres=gpu:v100:4,nvme:50 -t 15 --mem=0
 #SBATCH -A project_2001659
 #SBATCH --output=logs/slurm-%x-%j.out
 
-PYTHON=python3
-if [ -n "$SING_IMAGE" ]; then
-    PYTHON="singularity_wrapper exec python3"
-    echo "Using Singularity image $SING_IMAGE"
-fi
-
-MAIN_PY=horovod/examples/pytorch/pytorch_imagenet_resnet50.py
 DATASET_TAR_ARCHIVE=/scratch/dac/data/ilsvrc2012-torch-resized-new.tar
-DATADIR=$LOCAL_SCRATCH
 
 module list
-export NCCL_DEBUG=INFO
+#export NCCL_DEBUG=INFO
 
 set -x
 
@@ -22,9 +14,12 @@ date
 hostname
 nvidia-smi
 
-$PYTHON -c "import sys; import torch; print(sys.version, '\nPyTorch:', torch.__version__)"
+rm -f checkpoint-*.pth.tar
 
-tar xf $DATASET_TAR_ARCHIVE --strip 1 -C $LOCAL_SCRATCH/
+tar xf $DATASET_TAR_ARCHIVE --strip 1 -C $LOCAL_SCRATCH
 
-srun $PYTHON $MAIN_PY --train-dir=${DATADIR}/train --val-dir=${DATADIR}/val --epochs=1
+srun python3 pytorch_imagenet_resnet50_horovod_benchmark.py \
+     --train-dir=${LOCAL_SCRATCH}/train \
+     --val-dir=${LOCAL_SCRATCH}/val \
+     --epochs=2
 date
--- pytorch-horovod-imagenet-gpu8.sh
+++ pytorch-horovod-imagenet-gpu8.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=2 --ntasks=8 --cpus-per-task=10 -p gputest --gres=gpu:v100:4,nvme:50 -t 15 --mem=32G
+#SBATCH --nodes=2 --ntasks=8 --cpus-per-task=10 -p gputest --gres=gpu:v100:4,nvme:50 -t 15 --mem=0
 #SBATCH -A project_2001659
 #SBATCH --output=logs/slurm-%x-%j.out
 
-MAIN_PY=horovod/examples/pytorch/pytorch_imagenet_resnet50.py
 DATASET_TAR_ARCHIVE=/scratch/dac/data/ilsvrc2012-torch-resized-new.tar
-DATADIR=$LOCAL_SCRATCH
 
 module list
 #export NCCL_DEBUG=INFO
@@ -16,8 +14,13 @@ date
 hostname
 nvidia-smi
 
+rm -f checkpoint-*.pth.tar
+
 srun --ntasks=$SLURM_NNODES --ntasks-per-node=1 \
      tar xf $DATASET_TAR_ARCHIVE --strip 1 -C $LOCAL_SCRATCH/
 
-srun python3 $MAIN_PY --train-dir=${DATADIR}/train --val-dir=${DATADIR}/val --epochs=2
+srun python3 pytorch_imagenet_resnet50_horovod_benchmark.py \
+     --train-dir=${LOCAL_SCRATCH}/train \
+     --val-dir=${LOCAL_SCRATCH}/val \
+     --epochs=2
 date
