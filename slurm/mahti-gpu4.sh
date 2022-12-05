780b8598fb5375ff1e81faae4c48bc651cb28fd5
Mats Sjöberg <mats.sjoberg@csc.fi>
2022-12-05 11:58:34 +0200

    Fix to mahti-gpu4.sh script

--- slurm/mahti-gpu4.sh
+++ slurm/mahti-gpu4.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --account=project_2001659
 #SBATCH --partition=gputest
-#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=128
 #SBATCH --time=15
 #SBATCH --gres=gpu:a100:4,nvme:200
 #SBATCH --output=logs/slurm-%x-%j.out
