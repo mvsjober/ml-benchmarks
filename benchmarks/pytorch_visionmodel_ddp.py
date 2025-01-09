38a47ac87ad51c98edf09efae8498dbd3327bb67
Mats Sjöberg <mats.sjoberg@csc.fi>
2025-01-09 16:39:38 +0200

    Further LUMI fixes

--- benchmarks/pytorch_visionmodel_ddp.py
+++ benchmarks/pytorch_visionmodel_ddp.py
@@ -1,9 +1,12 @@
 # Based on multiprocessing example from
 # https://yangkky.github.io/2019/07/08/distributed-pytorch-tutorial.html
 
+import multiprocessing
 from datetime import datetime
 import argparse
 import os
+import psutil
+
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -16,6 +19,25 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torchvision import models
 
+def set_cpu_affinity(rank, local_rank):
+    LUMI_GPU_CPU_map = {
+        # A mapping from GCD to the closest CPU cores in a LUMI-G node
+        # Note that CPU cores 0, 8, 16, 24, 32, 40, 48, 56 are reserved for the
+        # system and not available for the user
+        # See https://docs.lumi-supercomputer.eu/hardware/lumig/
+        0: [49, 50, 51, 52, 53, 54, 55],
+        1: [57, 58, 59, 60, 61, 62, 63],
+        2: [17, 18, 19, 20, 21, 22, 23],
+        3: [25, 26, 27, 28, 29, 30, 31],
+        4: [1, 2, 3, 4, 5, 6, 7],
+        5: [9, 10, 11, 12, 13, 14, 15],
+        6: [33, 34, 35, 36, 37, 38, 39],
+        7: [41, 42, 43, 44, 45, 46, 47],
+    }
+    cpu_list = LUMI_GPU_CPU_map[local_rank]
+    print(f"Rank {rank} (local {local_rank}) binding to cpus: {cpu_list}")
+    psutil.Process().cpu_affinity(cpu_list)
+    
 
 class SyntheticData(torch.utils.data.Dataset):
     def __init__(self,
@@ -62,11 +84,15 @@ def dataset_from_datadir(datadir, verbose=True):
 
 
 def train(args):
+    rank = int(os.environ['RANK'])
     local_rank = int(os.environ['LOCAL_RANK'])
-    verbose = local_rank == 0
+    verbose = rank == 0
     if verbose:
         print('Using PyTorch version:', torch.__version__)
         print(torch.__config__.show())
+
+    if args.set_cpu_binds:
+        set_cpu_affinity(rank, local_rank)
 
     dist.init_process_group(backend='nccl')
     world_size = dist.get_world_size()
@@ -91,7 +117,7 @@ def train(args):
                               shuffle=False, num_workers=args.workers,
                               pin_memory=True, sampler=train_sampler)
 
-    scaler = torch.cuda.amp.GradScaler(enabled=args.fp16)
+    scaler = torch.amp.GradScaler('cuda', enabled=args.fp16)
     if verbose and args.fp16:
         print(f"Using fp16 (PyTorch automatic mixed precision)")
 
@@ -112,7 +138,7 @@ def train(args):
         )
         prof.start()
 
-    total_step = args.steps if args.steps is not None else len(train_loader)
+    total_steps = args.steps if args.steps is not None else len(train_loader)
 
     if args.mlflow and verbose:
         import mlflow
@@ -137,17 +163,22 @@ def train(args):
     # For final average
     avg_images = 0
     avg_start = None
-    tot_steps = 0
+    avg_stop = None
+    steps_counter = 0
+
+    real_start = datetime.now()
+
+    if args.warmup_steps == 0:
+        avg_start = datetime.now()
 
     for epoch in range(args.epochs):
         for i, (images, labels) in enumerate(train_loader):
             images = images.cuda(non_blocking=True)
             labels = labels.cuda(non_blocking=True)
 
-            with torch.cuda.amp.autocast(enabled=args.fp16):
+            with torch.amp.autocast('cuda', enabled=args.fp16):
                 outputs = model(images)
                 loss = criterion(outputs, labels)
-
 
             scaler.scale(loss).backward()
             scaler.step(optimizer)
@@ -159,11 +190,19 @@ def train(args):
             li = len(images)
             last_images += li
 
-            tot_steps += 1
-            if tot_steps == args.warmup_steps:
+            steps_counter += 1
+
+            # Start time counter for final average if we have processed all
+            # warmup steps
+            if steps_counter == args.warmup_steps:
                 avg_start = datetime.now()
-            elif tot_steps > args.warmup_steps:
-                avg_images += li
+            # After warmup steps add image count to avg_images counter
+            elif steps_counter > args.warmup_steps:
+                # we also exclude the last batch as that has some additional
+                # delays which affects the average
+                if steps_counter < total_steps-1:
+                    avg_images += li
+                    avg_stop = datetime.now()
 
             if (i + 1) % args.print_steps == 0 and verbose:
                 now = datetime.now()
@@ -177,7 +216,7 @@ def train(args):
                         "images/sec": last_images*world_size/last_secs
                         })
 
-                print(f'Epoch [{epoch+1}/{args.epochs}], Step [{i+1}/{total_step}], '
+                print(f'Epoch [{epoch+1}/{args.epochs}], Step [{i+1}/{total_steps}], '
                       f'Loss: {loss.item():.4f}, '
                       f'Images/sec: {last_images*world_size/last_secs:.2f} '
                       f'(last {args.print_steps} steps)')
@@ -195,8 +234,11 @@ def train(args):
                 #         }, cp_fname)
                 #     mlflow.log_artifact(cp_fname, artifact_path='checkpoints')
 
-            if args.steps is not None and tot_steps >= args.steps:
+            if args.steps is not None and steps_counter >= args.steps:
                 break
+
+    dur = datetime.now() - real_start
+    avg_dur = (avg_stop - avg_start).total_seconds()
 
     if args.profiler:
         if args.profiler_format == 'json' and verbose:
@@ -211,13 +253,14 @@ def train(args):
         if avg_start is None:
             print("WARNING: stopped before warmup steps done, not printing stats.")
         else:
-            dur = datetime.now() - avg_start
             print(f"Training completed in: {dur}")
-            print(f"Images/sec: {avg_images*world_size/dur.total_seconds():.2f} "
+            print(f"Images/sec: {avg_images*world_size/avg_dur:.2f} "
                   f"(average, skipping {args.warmup_steps} warmup steps)")
 
 
 def main():
+    multiprocessing.set_start_method('spawn')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', default=1, type=int, metavar='N',
                         help='number of total epochs to run')
@@ -240,8 +283,10 @@ def main():
     parser.add_argument('--mlflow', nargs='?', type=str, const='./mlruns')
     parser.add_argument('--fp16', action='store_true', default=False,
                         help='enable mixed precision')
-    args = parser.parse_args()
+    parser.add_argument('--set-cpu-binds', default=False, action="store_true",
+                        help='Bind the process to the CPU cores closest to the GPU used by the process (LUMI only).')
 
+    args = parser.parse_args()
     train(args)
 
 
